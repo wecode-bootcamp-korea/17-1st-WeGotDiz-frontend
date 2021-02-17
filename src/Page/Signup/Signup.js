625478d37439028f214ebee9bcdf47151e715762
허혜성 <heohyeseong@heohyeseong-ui-MacBookAir.local>
2021-02-17 14:07:03 +0900

    ADD:회원가입구현중

--- src/Page/Signup/Signup.js
+++ src/Page/Signup/Signup.js
@@ -1,13 +1,70 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import './Signup.scss';
 
 class Signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      checked: false,
+      email: '',
+      // name: '',
+      // pw: '',
+      // pwCheck: '',
+      emailError: '',
+    };
+  }
+  //체크박스 확인
+  handleCheck = e => {
+    this.setState({ checked: e.target.checked });
+  };
+
+  //이메일 입력 확인
+  handleInput = e => {
+    const { value, name } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  isEmail = email => {
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email);
+  };
+
+  onTextValidation = () => {
+    let emailError = '';
+
+    if (!this.isEmail(this.state.email)) {
+      emailError = '이메일 형식이 올바르지 않습니다.';
+    }
+    if (emailError === '') {
+      this.setState({ emailError });
+      return false;
+    }
+    return true;
+  };
+
+  onSubmitEmail = e => {
+    e.preventDefault();
+    const emailValid = this.onTextValidation();
+    if (!emailValid) {
+      console.log('check', this.state);
+    }
+  };
+
   render() {
+    console.log(this.state);
     return (
       <div className="signUpContainer">
         <h1>회원가입</h1>
         <div className="agreement">
-          <input type="checkbox" id="agreementCheckbox" />
+          <input
+            type="checkbox"
+            id="agreementCheckbox"
+            checkbox={this.state.checkde}
+            onChange={this.handleCheck}
+          />
           <label for="inputCheckbox" className="checkboxText">
             전체 동의
           </label>
@@ -16,23 +73,48 @@ class Signup extends Component {
           </p>
           <p>마케팅 정보 수신 동의(선택)</p>
         </div>
-        <from className="signUpEmail">
-          <input className="emailInput" type="text" placeholder="이메일 계정" />
+
+        <form className="signUpEmail" onSubmit={this.onSubmitEmail}>
+          <input
+            className="emailInput"
+            type="text"
+            placeholder="이메일 계정"
+            name="email"
+            onChange={this.handleInput}
+          />
           <button className="emailSubmit">인증하기</button>
           <p>위 이메일로 인증번호가 발송됩니다.</p>
-        </from>
-        <form>
-          <input className="nameInput" type="text" placeholder="이름" />
+          <div style={{ color: 'red' }}>{this.state.emailError}</div>
         </form>
-        <from className="passwordInput">
-          <input type="password" placeholder="비밀번호 입력" />
-          <input type="password" placeholder="비밀번호 확인" />
+
+        <form>
+          <input
+            className="nameInput"
+            type="text"
+            placeholder="이름"
+            name="name"
+            onChange={this.handleInput}
+          />
+        </form>
+        <form className="passwordInput">
+          <input
+            type="password"
+            placeholder="비밀번호 입력"
+            name="pw"
+            onChange={this.handleInput}
+          />
+          <input
+            type="password"
+            placeholder="비밀번호 확인"
+            name="pwCheck"
+            onChange={this.handleInput}
+          />
           <p>영문, 숫자, 특수문자 (!@#$%^&*+=-)를 조합한 8자 이상</p>
-        </from>
+        </form>
         <button className="recommendBtn">완료</button>
       </div>
     );
   }
 }
 
-export default Signup;
+export default withRouter(Signup);

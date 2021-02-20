28c695c861a8ad9ebf9a2d1a3f55e7f2dccda98d
93jm <65124480+93jm@users.noreply.github.com>
2021-02-20 16:24:53 +0900

    MODIFY : nav bar 수정중

--- src/Component/Nav/Nav.js
+++ src/Component/Nav/Nav.js
@@ -6,9 +6,12 @@ class Nav extends Component {
   constructor() {
     super();
     this.state = {
+      isloggedIn: true,
       isNavSearch: false,
       isSuggestLeft: true,
       isSuggestRight: false,
+      searchText: '',
+      searchArr: [],
     };
   }
 
@@ -44,7 +47,26 @@ class Nav extends Component {
     }
   };
 
+  //검색 값 담기
+  handleSearchChange = e => {
+    this.setState({
+      searchText: e.target.value,
+    });
+  };
+
+  addSearch = () => {
+    const { searchText, searchArr } = this.state;
+
+    const newComment = {
+      searchId: Date.now(),
+      searchText: searchText,
+    };
+
+    const newComments = [...searchArr, newComment];
+  };
+
   render() {
+    const { isNavSearch, searchText, isloggedIn } = this.state;
     return (
       <div>
         <header className="navContainer">
@@ -80,14 +102,19 @@ class Nav extends Component {
                   <input
                     name="isNavSearch"
                     className="navSearch"
-                    onFocus={this.handleSuggestDisplayOn}
-                    onBlur={this.handleSuggestDisplayOff}
+                    onClick={this.handleSuggestDisplayOn}
+                    onChange={this.handleSearchChange}
+                    value={searchText}
+                    // onBlur={this.handleSuggestDisplayOff}
                     placeholder="어떤 프로젝트를 찾고 계신가요?"
                   />
                 </form>
                 <div
-                  className="navSearchSuggestOpen"
-                  style={!this.state.isNavSearch ? { display: 'none' } : null}
+                  className={
+                    !isNavSearch
+                      ? 'navSearchSuggestClose'
+                      : 'navSearchSuggestOpen'
+                  }
                 >
                   <div className="suggestLayer">
                     <div className="suggestLayerTab">
@@ -123,66 +150,6 @@ class Nav extends Component {
                               <i className="fas fa-times" />
                             </button>
                           </li>
-                          <li className="suggestContent">
-                            <span>피카츄 라이츄</span>
-                            <button className="suggestContentBtn">
-                              <i className="fas fa-times" />
-                            </button>
-                          </li>
-                          <li className="suggestContent">
-                            <span>피카츄 라이츄</span>
-                            <button className="suggestContentBtn">
-                              <i className="fas fa-times" />
-                            </button>
-                          </li>
-                          <li className="suggestContent">
-                            <span>피카츄 라이츄</span>
-                            <button className="suggestContentBtn">
-                              <i className="fas fa-times" />
-                            </button>
-                          </li>
-                          <li className="suggestContent">
-                            <span>피카츄 라이츄</span>
-                            <button className="suggestContentBtn">
-                              <i className="fas fa-times" />
-                            </button>
-                          </li>
-                          <li className="suggestContent">
-                            <span>피카츄 라이츄</span>
-                            <button className="suggestContentBtn">
-                              <i className="fas fa-times" />
-                            </button>
-                          </li>
-                          <li className="suggestContent">
-                            <span>피카츄 라이츄</span>
-                            <button className="suggestContentBtn">
-                              <i className="fas fa-times" />
-                            </button>
-                          </li>
-                          <li className="suggestContent">
-                            <span>피카츄 라이츄</span>
-                            <button className="suggestContentBtn">
-                              <i className="fas fa-times" />
-                            </button>
-                          </li>
-                          <li className="suggestContent">
-                            <span>피카츄 라이츄</span>
-                            <button className="suggestContentBtn">
-                              <i className="fas fa-times" />
-                            </button>
-                          </li>
-                          <li className="suggestContent">
-                            <span>피카츄 라이츄</span>
-                            <button className="suggestContentBtn">
-                              <i className="fas fa-times" />
-                            </button>
-                          </li>
-                          <li className="suggestContent">
-                            <span>피카츄 라이츄</span>
-                            <button className="suggestContentBtn">
-                              <i className="fas fa-times" />
-                            </button>
-                          </li>
                         </ul>
                       </div>
                     </div>
@@ -191,17 +158,36 @@ class Nav extends Component {
                         <i className="far fa-trash-alt" />
                         전체삭제
                       </button>
-                      <button className="suggestListClosed">닫기</button>
+                      <button
+                        className="suggestListClosed"
+                        onClick={() => {
+                          this.setState({
+                            isNavSearch: false,
+                          });
+                        }}
+                      >
+                        닫기
+                      </button>
                     </div>
                   </div>
                 </div>
               </div>
+              {isloggedIn && (
+                <span className="myPage">
+                  <Link to="/mypage"></Link>
+                </span>
+              )}
+
               <span className="navJoin">
-                <Link to="/login">로그인</Link>
+                <Link to={isloggedIn ? '/' : '/login'}>
+                  {isloggedIn ? '로그아웃' : '로그인'}
+                </Link>
               </span>
-              <span className="navJoin">
-                <Link to="/signup">회원가입</Link>
-              </span>
+              {!isloggedIn && (
+                <span className="navJoin">
+                  <Link to="/signup">회원가입</Link>
+                </span>
+              )}
             </div>
           </div>
         </header>

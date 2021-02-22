60a420a393a709ccc3f91a86ad5962803bb697ef
허혜성 <hye.ss929@gmail.com>
2021-02-22 20:05:07 +0900

    ADD:api주소변경

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -76,7 +76,7 @@ class Signup extends Component {
 
   goToMain = () => {
     // e.preventDefault();
-    fetch('http://192.168.43.45/user/signup', {
+    fetch('http://10.58.2.95:8000/user/signup', {
       method: 'POST',
       body: JSON.stringify({
         email: this.state.email,
